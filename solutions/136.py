0def6eeb4035ecb48705eebd8265f70d9e10e54b
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:49:22 +0300

    feat(solutions): add Single Number problem
    See: https://leetcode.com/problems/single-number/

---
+++ solutions/136.py
@@ -0,0 +1,23 @@
+"""`136. Single Number <https://leetcode.com/problems/single-number/>`_
+
+>>> Solution().singleNumber([2, 2, 1])
+1
+
+>>> Solution().singleNumber([4, 1, 2, 1, 2])
+4
+
+>>> Solution().singleNumber([1])
+1
+"""
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """Finds single number."""
+        counter = Counter(nums)
+        for num in counter.keys():
+            if counter[num] == 1:
+                return num
