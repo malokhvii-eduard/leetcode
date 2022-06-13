8ea5c92639eef182e0cc238cc017b9e90460d95e
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 20:05:17 +0300

    feat(solutions): add XOR Operation in an Array problem
    See: https://leetcode.com/problems/xor-operation-in-an-array/

---
+++ solutions/1486.py
@@ -0,0 +1,23 @@
+"""`1486. XOR Operation in an Array <https://leetcode.com/problems/xor-operation-in-an-array/>`_
+
+>>> Solution().xorOperation(5, 0)
+8
+
+>>> Solution().xorOperation(4, 3)
+8
+
+>>> Solution().xorOperation(1, 7)
+7
+"""
+
+from functools import reduce
+from operator import xor
+
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        """Returns the bitwise XOR of all elements of nums."""
+        if n == 1:
+            return start
+
+        return reduce(xor, range(start, start + n * 2, 2))
