2031d497851ec5112c3a2e0bdfab97793785310f
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:57:02 +0300

    feat(solutions): add Power of Two problem
    See: https://leetcode.com/problems/power-of-two/

---
+++ solutions/231.py
@@ -0,0 +1,23 @@
+"""`231. Power of Two <https://leetcode.com/problems/power-of-two/>`_
+
+>>> Solution().isPowerOfTwo(1)
+True
+
+>>> Solution().isPowerOfTwo(16)
+True
+
+>>> Solution().isPowerOfTwo(3)
+False
+"""
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        """Returns true if it is a power of two, and false otherwise."""
+        if n < 1:
+            return False
+
+        while n % 2 == 0:
+            n /= 2
+
+        return n == 1
