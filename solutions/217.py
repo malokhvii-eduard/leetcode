14330741cccd78c0d856438a822f9bd1b47ff87d
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:56:04 +0300

    feat(solutions): add Contains Duplicate problem
    See: https://leetcode.com/problems/contains-duplicate/

---
+++ solutions/217.py
@@ -0,0 +1,28 @@
+"""`217. Contains Duplicate <https://leetcode.com/problems/contains-duplicate/>`_
+
+>>> Solution().containsDuplicate([1, 2, 3, 1])
+True
+
+>>> Solution().containsDuplicate([1, 2, 3, 4])
+False
+
+>>> Solution().containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
+True
+"""
+
+
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        """Returns true if any value appears at least twice in the array, and returns
+        false if every element is distinct."""
+        counter = defaultdict(int)
+        for num in nums:
+            counter[num] += 1
+            if num in counter and counter[num] > 1:
+                return True
+
+        return False
