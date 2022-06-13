3eb3a40b85252064d12d41e16451e5c53e08a57e
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:48:20 +0300

    feat(solutions): add Pascal's Triangle II problem
    See: https://leetcode.com/problems/pascals-triangle-ii/

---
+++ solutions/119.py
@@ -0,0 +1,36 @@
+"""`119. Pascal's Triangle II <https://leetcode.com/problems/pascals-triangle-ii/>`_
+
+>>> Solution().getRow(3)
+[1, 3, 3, 1]
+
+>>> Solution().getRow(0)
+[1]
+
+>>> Solution().getRow(1)
+[1, 1]
+
+>>> Solution().getRow(4)
+[1, 4, 6, 4, 1]
+"""
+
+from typing import List
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        """Returns the row of the Pascal's triangle."""
+        n = rowIndex + 1
+        if n == 1:
+            return [1]
+
+        previous = [1, 1]
+        for row in range(2, n):
+            current = [1]
+
+            for i, j in zip(range(len(previous) - 1), range(1, len(previous))):
+                current.append(previous[i] + previous[j])
+
+            current.append(1)
+            previous = current
+
+        return previous
