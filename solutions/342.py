2adb6ee390bbb371ad86aef2408b04b12ecf7424
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 20:00:56 +0300

    feat(solutions): add Power of Four problem
    See: https://leetcode.com/problems/power-of-four/

---
+++ solutions/342.py
@@ -0,0 +1,23 @@
+"""`342. Power of Four <https://leetcode.com/problems/power-of-four/>`_
+
+>>> Solution().isPowerOfFour(16)
+True
+
+>>> Solution().isPowerOfFour(5)
+False
+
+>>> Solution().isPowerOfFour(1)
+True
+"""
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        """Returns true if it is a power of four, and false otherwise."""
+        if n < 1:
+            return False
+
+        while n % 4 == 0:
+            n /= 4
+
+        return n == 1
