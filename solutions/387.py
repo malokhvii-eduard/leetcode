44a6cf3760e42fbbc557f406abaa5ee3feb359df
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 20:02:50 +0300

    feat(solutions): add First Unique Character in a String problem
    See: https://leetcode.com/problems/first-unique-character-in-a-string/

---
+++ solutions/387.py
@@ -0,0 +1,24 @@
+"""`387. First Unique Character in a String <https://leetcode.com/problems/first-unique-character-in-a-string/>`_
+
+>>> Solution().firstUniqChar("leetcode")
+0
+
+>>> Solution().firstUniqChar("loveleetcode")
+2
+
+>>> Solution().firstUniqChar("aabb")
+-1
+"""
+
+from collections import Counter
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """Returns index of first non-repeating character."""
+        counter = Counter(s)
+        for i, c in enumerate(s):
+            if counter[c] == 1:
+                return i
+
+        return -1
