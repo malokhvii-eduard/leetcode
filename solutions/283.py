f50bbb518a18e7ea60280e5ae8caaa74666359a9
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:59:12 +0300

    feat(solutions): add Move Zeroes problem
    See: https://leetcode.com/problems/move-zeroes/

---
+++ solutions/283.py
@@ -0,0 +1,30 @@
+"""`283. Move Zeroes <https://leetcode.com/problems/move-zeroes/>`_
+
+>>> nums = [0, 1, 0, 3, 12]
+>>> Solution().moveZeroes(nums)
+>>> nums
+[1, 3, 12, 0, 0]
+
+>>> nums = [0]
+>>> Solution().moveZeroes(nums)
+>>> nums
+[0]
+"""
+
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """Moves all 0's to the end."""
+        if len(nums) < 1:
+            return
+
+        j = 0
+        for num in nums:
+            if num != 0:
+                nums[j] = num
+                j += 1
+
+        for i in range(j, len(nums)):
+            nums[i] = 0
