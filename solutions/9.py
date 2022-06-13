6929ed1f306073a58e75335eb013f7284c5822b4
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:33:10 +0300

    feat(solutions): add Palindrome Number problem
    See: https://leetcode.com/problems/palindrome-number/

---
+++ solutions/9.py
@@ -0,0 +1,31 @@
+"""`9. Palindrome Number <https://leetcode.com/problems/palindrome-number/>`_
+
+>>> Solution().isPalindrome(121)
+True
+
+>>> Solution().isPalindrome(1001)
+True
+
+>>> Solution().isPalindrome(0)
+True
+
+>>> Solution().isPalindrome(-121)
+False
+
+>>> Solution().isPalindrome(10)
+False
+"""
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        """Returns true if x is palindrome integer."""
+        if x < 0:
+            return False
+
+        digits = list(map(int, str(x)))
+        for i in range(len(digits) // 2):
+            if digits[i] != digits[len(digits) - 1 - i]:
+                return False
+
+        return True
