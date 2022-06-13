9865fa2bbdf98e462d274d6bb5886626ed46da8a
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:38:11 +0300

    feat(solutions): add Remove Duplicates from Sorted Array problem
    See: https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ solutions/26.py
@@ -0,0 +1,29 @@
+"""`26. Remove Duplicates from Sorted Array <https://leetcode.com/problems/remove-duplicates-from-sorted-array/>`_
+
+>>> Solution().removeDuplicates([1])
+1
+
+>>> Solution().removeDuplicates([1, 1, 2])
+2
+
+>>> Solution().removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+5
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """Removes the duplicates in-place."""
+        if len(nums) < 2:
+            return len(nums)
+
+        previous = 0
+        for current in range(1, len(nums)):
+            if nums[current] != nums[previous]:
+                previous += 1
+                nums[previous] = nums[current]
+
+        return previous + 1
