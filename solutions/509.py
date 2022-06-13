52369809041f299b506a28139eea3758cff414e2
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 20:04:15 +0300

    feat(solutions): add Fibonacci Number problem
    See: https://leetcode.com/problems/fibonacci-number/

---
+++ solutions/509.py
@@ -0,0 +1,24 @@
+"""`509. Fibonacci Number <https://leetcode.com/problems/fibonacci-number/>`_
+
+>>> Solution().fib(2)
+1
+
+>>> Solution().fib(3)
+2
+
+>>> Solution().fib(4)
+3
+
+>>> Solution().fib(30)
+832040
+"""
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        """Calculates F(N), where F(0) = 0, F(1) = 1, F(n) = F(n - 1) + F(n - 2)."""
+        x, y = 0, 1
+        for _ in range(n):
+            x, y = y, x + y
+
+        return x
