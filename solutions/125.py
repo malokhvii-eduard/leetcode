6b7738a9681840d033bc3c8ff44ffa0165cb3e54
Eduard Malokhvii <malokhvii.ee@gmail.com>
2022-06-13 19:48:54 +0300

    feat(solutions): add Valid Palindrome problem
    See: https://leetcode.com/problems/valid-palindrome/

---
+++ solutions/125.py
@@ -0,0 +1,23 @@
+"""`125. Valid Palindrome <https://leetcode.com/problems/valid-palindrome/>`_
+
+>>> Solution().isPalindrome("A man, a plan, a canal: Panama")
+True
+
+>>> Solution().isPalindrome("race a car")
+False
+
+>>> Solution().isPalindrome(" ")
+True
+"""
+
+
+import string
+
+
+class Solution:
+    PRINTABLE = str.maketrans("", "", string.whitespace + string.punctuation)
+
+    def isPalindrome(self, s: str) -> bool:
+        """Returns true if it is a palindrome, or false otherwise."""
+        s = s.translate(self.PRINTABLE).lower()
+        return s == s[::-1]
