8f04236d8f9ab1f9ab17ed1e221cd2f28d84aa3a
Eduard Malokhvii <malokhvii.ee@gmail.com>
2023-03-22 00:27:44 +0200

    feat(solutions): add Excel Sheet Column Title problem
    See: https://leetcode.com/problems/excel-sheet-column-title/

---
+++ solutions/168.py
@@ -0,0 +1,29 @@
+"""`168. Excel Sheet Column Title <https://leetcode.com/problems/excel-sheet-column-title/>`_
+
+>>> Solution().convertToTitle(1)
+'A'
+
+>>> Solution().convertToTitle(28)
+'AB'
+
+>>> Solution().convertToTitle(701)
+'ZY'
+
+>>> Solution().convertToTitle(2**31 - 1)
+'FXSHRXW'
+"""
+
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        """Converts integer column number to corresponding column title as it
+        appears in an Excel sheet."""
+        letters = []
+        while columnNumber > 0:
+            quotient, remainder = divmod(columnNumber - 1, 26)
+
+            letters.append(chr(remainder + 65))
+
+            columnNumber = quotient
+
+        return "".join(reversed(letters))
