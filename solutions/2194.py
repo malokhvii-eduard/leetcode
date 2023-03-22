8184b74caa186eef8dc419e003ebb3100b19a211
Eduard Malokhvii <malokhvii.ee@gmail.com>
2023-03-22 21:57:09 +0200

    feat(solutions): add Cells in a Range on an Excel Sheet problem
    See: https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/

---
+++ solutions/2194.py
@@ -0,0 +1,19 @@
+"""`2194. Cells in a Range on an Excel Sheet <https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/>`_
+
+>>> list(Solution().cellsInRange('K1:L2'))
+['K1', 'K2', 'L1', 'L2']
+
+>>> list(Solution().cellsInRange('A1:F1'))
+['A1', 'B1', 'C1', 'D1', 'E1', 'F1']
+"""
+
+from typing import Generator
+
+
+class Solution:
+    def cellsInRange(self, s: str) -> Generator[str, None, None]:
+        """Returns the list of cells in a spreadsheet range."""
+        col1, row1, col2, row2 = s[0], s[1], s[3], s[4]
+        for col in range(ord(col1), ord(col2) + 1):
+            for row in range(int(row1), int(row2) + 1):
+                yield f"{chr(col)}{row}"
