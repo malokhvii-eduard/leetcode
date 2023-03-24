cb84d53b0c67aa0a159b1c7dfb5d83ce2694489d
Eduard Malokhvii <malokhvii.ee@gmail.com>
2023-03-24 13:18:54 +0200

    feat(solutions): add Isomorphic Strings problem
    See: https://leetcode.com/problems/isomorphic-strings/

---
+++ solutions/205.py
@@ -0,0 +1,31 @@
+"""`205. Isomorphic Strings <https://leetcode.com/problems/isomorphic-strings/>`_
+
+>>> Solution().isIsomorphic("egg", "add")
+True
+
+>>> Solution().isIsomorphic("foo", "bar")
+False
+
+>>> Solution().isIsomorphic("paper", "title")
+True
+
+>>> Solution().isIsomorphic("badc", "baba")
+False
+"""
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        """Determines if two strings are isomorphic."""
+        sm, tm = {}, {}
+        for cs, ct in zip(s, t):
+            if cs not in sm:
+                sm[cs] = ct
+
+            if ct not in tm:
+                tm[ct] = cs
+
+            if sm[cs] != ct or tm[ct] != cs:
+                return False
+
+        return True
