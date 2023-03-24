90bce25e73bd9966d8456b46088c04741cc860c4
Eduard Malokhvii <malokhvii.ee@gmail.com>
2023-03-24 13:00:31 +0200

    feat(solutions): add Length of Last Word problem
    See: https://leetcode.com/problems/length-of-last-word/

---
+++ solutions/58.py
@@ -0,0 +1,20 @@
+"""`58. Length of Last Word <https://leetcode.com/problems/length-of-last-word/>`_
+
+>>> Solution().lengthOfLastWord("Hello World")
+5
+
+>>> Solution().lengthOfLastWord("   fly me   to   the moon  ")
+4
+
+>>> Solution().lengthOfLastWord("luffy is still joyboy")
+6
+"""
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        """Returns the length of the last word in the string."""
+        s = s.rstrip()
+        # fmt: off
+        return len(s[s.rfind(" ") + 1:])
+        # fmt: on
